fbbefe3434b4de0e6479d4b60ca80349f33b58eb
Erik <erik90781@gmail.com>
2023-05-27 18:52:34 +0200

    +error file created for invalid URL's & added login/typespeedtest folder/url

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		if ($page.status.toString().startsWith('3') || $page.status.toString().startsWith('4')) {
+			goto('/typespeedtest');
+		}
+	});
+</script>
+
+<h1>{$page.status}: {$page.error?.message}</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+	// Start of UI language imports
+
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	// End of UI language imports
+
+	import { goto } from '$app/navigation';
+
+	function goToTypespeedtest(): void {
+		goto('/typespeedtest');
+	}
 </script>
 
 <div class="container mx-auto flex justify-left items-left m-5">
-	<h1 class="text-4xl font-bold text-left">typeOG</h1>
+	<h1
+		class="text-4xl font-bold text-left"
+		on:click={goToTypespeedtest}
+		on:keyup={goToTypespeedtest}
+	>
+		typeOG
+	</h1>
 </div>
 
 <slot />
