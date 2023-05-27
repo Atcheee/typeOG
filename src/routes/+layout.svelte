15861fc8f0e839ebce829a32874076554459f8fb
Erik <erik90781@gmail.com>
2023-05-27 17:28:58 +0200

    initial folder structure + input box started

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
@@ -6,5 +6,9 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 </script>
+
+<div class="container mx-auto flex justify-left items-left m-5">
+	<h1 class="text-4xl font-bold text-left">typeOG</h1>
+</div>
 
 <slot />
